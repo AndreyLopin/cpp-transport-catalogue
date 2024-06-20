2c568d2eb8384da7b48ac0a6a8d3ea62bf537dff
andrey96lopin <andrey96lopin@gmail.com>
2024-06-21 00:01:45 +0300

    Добавить файлы json и svg

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -1,4 +1,104 @@
 #pragma once
-/*
- * Место для вашей JSON-библиотеки
- */
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace json {
+
+class Node;
+// Сохраните объявления Dict и Array без изменения
+using Dict = std::map<std::string, Node>;
+using Array = std::vector<Node>;
+using Value = std::variant<std::nullptr_t, int, double, std::string, bool, Array, Dict>;
+
+// Эта ошибка должна выбрасываться при ошибках парсинга JSON
+class ParsingError : public std::runtime_error {
+public:
+    using runtime_error::runtime_error;
+};
+
+class Node {
+public:
+   /* Реализуйте Node, используя std::variant */
+    Node() : value_(nullptr) {};
+    
+
+    template <typename Type>
+    Node(Type value)
+        : value_(std::move(value)) {
+    }
+
+    const Value& GetValue() const {
+         return value_;
+    }
+
+    int AsInt() const;
+    bool AsBool() const;
+    double AsDouble() const;
+    const std::string& AsString() const;
+    const Array& AsArray() const;
+    const Dict& AsMap() const;
+
+    bool IsInt() const;
+    bool IsDouble() const;
+    bool IsPureDouble() const;
+    bool IsBool() const;
+    bool IsString() const;
+    bool IsNull() const;
+    bool IsArray() const;
+    bool IsMap() const;
+
+    bool operator== (const Node& other) const {
+        if (IsNull() && other.IsNull()) {
+            return true;
+        } else if (IsInt() && other.IsInt()) {
+            return AsInt() == other.AsInt();
+        } else if (IsPureDouble() && other.IsPureDouble()) {
+            return AsDouble() == other.AsDouble();
+        } else if (IsBool() && other.IsBool()) {
+            return AsBool() == other.AsBool();
+        } else if (IsString() && other.IsString()) {
+            return AsString() == other.AsString();
+        } else if (IsArray() && other.IsArray()) {
+            return AsArray() == other.AsArray();
+        } else if (IsMap() && other.IsMap()) {
+            return AsMap() == other.AsMap();
+        } else {
+            return false;
+        }
+    }
+
+private:
+    Value value_;
+};
+
+inline bool operator!=(const Node& lhs, const Node& rhs) {
+    return !(lhs == rhs);
+}
+
+class Document {
+public:
+    explicit Document(Node root);
+
+    const Node& GetRoot() const;
+
+private:
+    Node root_;
+};
+
+inline bool operator==(const Document& lhs, const Document& rhs) {
+    return lhs.GetRoot() == rhs.GetRoot();
+}
+
+inline bool operator!=(const Document& lhs, const Document& rhs) {
+    return !(lhs == rhs);
+}
+
+Document Load(std::istream& input);
+
+void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
--- transport-catalogue/svg.cpp
+++ transport-catalogue/svg.cpp
@@ -1,5 +1,175 @@
 #include "svg.h"
 
-/*
- * Место для вашей svg-библиотеки
- */
+namespace svg {
+
+using namespace std::literals;
+
+std::ostream& operator<< (std::ostream& out, const StrokeLineCap& line_cap) {
+    switch (line_cap) {
+        case StrokeLineCap::BUTT:
+            out << "butt"s;
+            break;
+        case StrokeLineCap::ROUND:
+            out << "round"s;
+            break;
+        case StrokeLineCap::SQUARE:
+            out << "square"s;
+            break;
+    }
+    return out;
+}
+
+std::ostream& operator << (std::ostream& out, const StrokeLineJoin& line_join) {
+    switch (line_join) {
+        case StrokeLineJoin::ARCS:
+            out << "arcs"s;
+            break;
+        case StrokeLineJoin::BEVEL:
+            out << "bevel"s;
+            break;
+        case StrokeLineJoin::MITER:
+            out << "miter"s;
+            break;
+        case StrokeLineJoin::MITER_CLIP:
+            out << "miter-clip"s;
+            break;
+        case StrokeLineJoin::ROUND:
+            out << "round"s;
+            break;
+    }
+    return out;
+}
+
+void Object::Render(const RenderContext& context) const {
+    context.RenderIndent();
+
+    // Делегируем вывод тега своим подклассам
+    RenderObject(context);
+
+    context.out << std::endl;
+}
+
+// ---------- Circle ------------------
+
+Circle& Circle::SetCenter(Point center)  {
+    center_ = center;
+    return *this;
+}
+
+Circle& Circle::SetRadius(double radius)  {
+    radius_ = radius;
+    return *this;
+}
+
+void Circle::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
+    out << "r=\""sv << radius_ << "\" "sv;
+    // Выводим атрибуты, унаследованные от PathProps
+    RenderAttrs(context.out);
+    out << "/>"sv;
+}
+
+// ------------- Polyline -----------------
+Polyline& Polyline::AddPoint(Point point) {
+    points_.push_back(point);
+    return *this;
+}
+
+void Polyline::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<polyline points=\""sv;
+    bool is_not_first = false;
+    for(const Point& p : points_) {
+        if(is_not_first) {
+            out << ' ';
+        }
+        is_not_first = true;
+        out << p.x << ',' << p.y;        
+    }
+    out << '\"';
+    // Выводим атрибуты, унаследованные от PathProps
+    RenderAttrs(context.out);
+    out << "/>"sv;
+}
+
+//-------------- Text ------------------------
+Text& Text::SetPosition(Point pos) {
+    pos_ = pos;
+    return *this;
+}
+
+Text& Text::SetOffset(Point offset) {
+    offset_ = offset;
+    return *this;
+}
+
+Text& Text::SetFontSize(uint32_t size) {
+    font_size_ = size;
+    return *this;
+}
+
+Text& Text::SetFontFamily(std::string font_family) {
+    font_family_ = std::move(font_family);
+    return *this;
+}
+
+Text& Text::SetFontWeight(std::string font_weight) {
+    font_weight_ = std::move(font_weight);
+    return *this;
+}
+
+
+Text& Text::SetData(std::string data) {
+    data_ = std::move(data);
+    return *this;
+}
+
+void Text::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<text x=\""sv << pos_.x << "\" y=\""sv << pos_.y;
+    out << "\" dx=\""sv << offset_.x << "\" dy=\""sv << offset_.y;
+    out << "\" font-size=\""sv << font_size_ << "\" "sv;
+    if(!font_family_.empty()) {
+        out << "font-family=\""sv << font_family_ << "\" "sv;
+    }
+    if(!font_weight_.empty()) {
+        out << "font-weight=\""sv << font_weight_ << "\" "sv;
+    }
+
+    // Выводим атрибуты, унаследованные от PathProps
+    RenderAttrs(context.out);
+
+    out << '>';
+
+    for(const char c : data_) {
+        if (c == '"') {
+            out << "&quot;";
+        } else if (c == '\'') {
+            out << "&apos;";
+        } else if (c == '<') {
+            out << "&lt;";
+        } else if (c == '>') {
+            out << "&gt;";
+        } else if (c == '&') {
+            out << "&amp;";
+        } else {
+            out << c;
+        }
+    }
+    out << "</text>"sv;
+}
+
+// ----------- Document -------------------
+void Document::Render(std::ostream& out) const {
+    out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"sv << std::endl;
+    out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"sv << std::endl;
+
+    for (const auto& el : objects_) {
+        el->Render(out);
+    }
+
+    out << "</svg>"sv;
+}
+
+}  // namespace svg
